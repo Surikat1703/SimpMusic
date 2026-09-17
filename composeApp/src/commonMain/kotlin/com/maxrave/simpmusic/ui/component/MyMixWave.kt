2deccbf23ede5b320e373f5433bf7e7771c4206d
vladi <vlad.lisaiev2007@gmail.com>
2026-09-17 19:01:27 +0300

    fix(mymix): missing mutableStateOf import in wave smoother

--- composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/MyMixWave.kt
+++ composeApp/src/commonMain/kotlin/com/maxrave/simpmusic/ui/component/MyMixWave.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
